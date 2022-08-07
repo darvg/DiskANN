c1bd369411d5f69294a16d69c836842ef62d3209
Siddharth Gollapudi <t-gollapudis@microsoft.com>
2022-08-07 17:46:39 +0530

    Add todos for stitched rewrite

--- tests/build_stitched_index_r.cpp
+++ tests/build_stitched_index_r.cpp
@@ -8,3 +8,13 @@ size_t random(size_t range_from, size_t range_to) {
   return distr(generator);
 }
 
+int main (int argc, char *argv[]) {
+	// TODO: 1. setup command line arguments (copy from old)
+	// TODO: 2. parse label file
+	// TODO: 3. for every label, collect points into map
+		// TODO: 3a. for every label, build unfiltered index
+	// TODO: 4. load the indices into memory and combine
+	// TODO: 5. adjust filesize
+	// TODO: 6. prune combined graph, then save
+}
+
