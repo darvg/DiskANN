039e7dc31062cf11e2b90fe3901da3279cb7501e
Siddharth Gollapudi <t-gollapudis@microsoft.com>
2022-08-25 17:55:07 +0530

    Add small fix for disk build

--- include/pq_flash_index.h
+++ include/pq_flash_index.h
@@ -95,13 +95,6 @@ namespace diskann {
 
     DISKANN_DLLEXPORT void cached_beam_search(
         const T *query, const _u64 k_search, const _u64 l_search, _u64 *res_ids,
-        float *res_dists, const _u64 beam_width, const bool use_filter,
-        const std::string &filter_label, 
-        const bool use_reorder_data = false, QueryStats *stats = nullptr);
-
-
-    DISKANN_DLLEXPORT void cached_beam_search(
-        const T *query, const _u64 k_search, const _u64 l_search, _u64 *res_ids,
         float *res_dists, const _u64 beam_width,
         const bool use_reorder_data = false, QueryStats *stats = nullptr);
 
